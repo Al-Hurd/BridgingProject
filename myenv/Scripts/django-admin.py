ea2fbcfd0fc257d0d0cbe656e1aaf2f0ec6f4ae9
Al-Hurd <al.hurdy@yahoo.co.uk>
2020-08-29 13:25:12 +0100

    First Commit for deployment

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lizzi\onedrive\documents\github\bridgingproject\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
